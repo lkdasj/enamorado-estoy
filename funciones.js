017901025ed793be97ccc0baf7e3c8efb2128663
ignacio-j3 <ignaciocovarrubias65@gmail.com>
2024-09-01 15:45:11 -0400

    Initial commit

---
+++ funciones.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    window.location.href = "respuesta_si.html";
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert("Espero que en algún momento puedas reconsiderarlo...");
+});
